9d11c920603f641284d2d3903f3351875759a482
Manas George <manas.george@gatech.edu>
2015-09-27 03:10:47 -0400

    Adding events to user response, adding schema.sql

--- Coffee.py
+++ Coffee.py
@@ -101,6 +101,9 @@ def get_user(user_id):
 		sql = "SELECT * FROM users WHERE profileid = %s"
 		cursor.execute(sql, (user_id,))
                 res = cursor.fetchone()
+		sql = "SELECT * FROM events WHERE user1 = %s OR user2 = %s"
+		cursor.execute(sql, (user_id,))
+                res = cursor.fetchone()
                 if(res is None):
                     return jsonify({'error':'No such user'})
 		return jsonify(res)
